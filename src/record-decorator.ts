9edb27329840bd72cf10d6cf35d02ac131cc5d26
BuildTools <unconfigured@null.spigotmc.org>
2020-01-24 13:11:02 -0600

    Made record publicly accessible and modifiable

--- src/record-decorator.ts
+++ src/record-decorator.ts
@@ -2,55 +2,50 @@ import { FileManager } from './file-manager'
 import { JsonDictionaryFile } from './json-dictionary-file'
 
 export class RecordDecorator<K extends string | number, V> {
-  constructor(private _record: Record<K, V> = {} as Record<K, V>) {}
+  constructor(public record: Record<K, V> = {} as Record<K, V>) {}
 
   public set(key: K, value: V) {
-    this._record[key] = value
+    this.record[key] = value
   }
 
   public get(key: K): V | undefined {
-    return Object(this._record)[key]
+    return Object(this.record)[key]
   }
 
   public has(key: K) {
-    return Object.keys(this._record).includes(key.toString())
+    return Object.keys(this.record).includes(key.toString())
   }
 
   public append(...records: Record<K, V>[]) {
     records.forEach(record => {
-      this._record = { ...Object(this._record), ...Object(record) }
+      this.record = { ...Object(this.record), ...Object(record) }
     })
     return this
   }
 
   public clear() {
-    this._record = {} as Record<K, V>
+    this.record = {} as Record<K, V>
     return this
   }
 
   public forEach(f: (k: K, v: V, i: number) => void) {
-    Object.keys(this._record).forEach((k, i) =>
-      f((k as unknown) as K, this._record[(k as unknown) as K], i)
+    Object.keys(this.record).forEach((k, i) =>
+      f((k as unknown) as K, this.record[(k as unknown) as K], i)
     )
   }
 
   public toMap() {
     const map = new Map<K, V>()
 
-    for (const key of Object.keys(this._record)) {
+    for (const key of Object.keys(this.record)) {
       map.set(key as K, this.get(key as K)!)
     }
 
     return map
   }
 
-  public get record() {
-    const r: Readonly<Record<K, V>> = this._record
-    return r
-  }
-
   public get length() {
-    return Object.keys(this._record).length
+    return Object.keys(this.record).length
   }
 
   public static fromFile<K extends number | string, V>(
